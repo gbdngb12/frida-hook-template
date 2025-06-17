736dd42c8797e33dd838851b7bda6341478d135a
gbdngb12 <gbdngb12@naver.com>
2025-06-17 11:54:42 +0900

    update offset

--- hook.py
+++ hook.py
@@ -4,23 +4,51 @@ import sys
 pid = int(sys.argv[1])
 
 def on_message(message, data):
-    print(f"[{message}] => {data}")
+    if message['type'] == 'send':
+        print(f"[+] {message['payload']}")
+    elif message['type'] == 'error':
+        print(f"[!] Error: {message['stack']}")
 
 # -------- Frida Script --------
 script_code = r"""
-var setdec_hook = Module.findExportByName("target.dll", "func");
-Interceptor.attach(setdec_hook, {
-	onEnter: function(args) {
-	
-	},
-	onLeave: function(retval) {
-	
-	}
-});
+// -------- Symbol-based Hook --------
+var symbol_addr = Module.findExportByName("target.dll", "func");
+if (symbol_addr !== null) {
+    Interceptor.attach(symbol_addr, {
+        onEnter: function(args) {
+            send("[symbol] onEnter called");
+        },
+        onLeave: function(retval) {
+            send("[symbol] onLeave called, retval: " + retval);
+        }
+    });
+} else {
+    send("[symbol] func not found in target.dll");
+}
+
+// -------- Offset-based Hook --------
+// 예: target.dll + 0x1234 (offset은 16진수로 전달 가능)
+var base = Module.findBaseAddress("target.dll");
+if (base !== null) {
+    var offset = 0x1234;  // 여기에 원하는 오프셋 입력
+    var target_addr = base.add(offset);
+    Interceptor.attach(target_addr, {
+        onEnter: function(args) {
+            send("[offset] onEnter called at offset 0x1234");
+        },
+        onLeave: function(retval) {
+            send("[offset] onLeave called, retval: " + retval);
+        }
+    });
+} else {
+    send("[offset] target.dll not found");
+}
 """
 
+# attach and load
 session = frida.attach(pid)
 script = session.create_script(script_code)
 script.on('message', on_message)
 script.load()
 sys.stdin.read()
+
